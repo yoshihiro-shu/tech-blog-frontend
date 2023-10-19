d8261201df7cf8099498f005840287c88a4af945
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-20 00:04:09 +0900

    fix: i18n config

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,8 @@ export default createMiddleware({
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: 'ja',
+
+  localeDetection: false,
 });
 
 export const config = {
