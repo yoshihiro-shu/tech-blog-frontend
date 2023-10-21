5cab4d2ff4d51f4ee6d3d6b903015ebd14566b2f
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-22 00:37:55 +0900

    add: undefined 対策

--- src/components/ArticleBox.tsx
+++ src/components/ArticleBox.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-// import { generatePath } from "react-router-dom";
-
 import Article from '../../server/types/article'
 
 type Props = { article: Article }
@@ -19,10 +17,10 @@ export const  ArticleBox = ({ article }: Props) => {
           <Image src={ article.thumbnailUrl } alt={article?.title} width={500} height={500}/>
         </Link>
         <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="/" className="text-blue-700 text-sm font-bold uppercase pb-4">{ article.category.name }</Link>
+          <Link href="/" className="text-blue-700 text-sm font-bold uppercase pb-4">{ article.category?.name }</Link>
           <Link href={articleDetailPath} className="text-3xl font-bold hover:text-gray-700 pb-4">{ article.title }</Link>
           <p className="text-sm pb-3">
-            By <Link href="/" className="font-semibold hover:text-gray-800">{ article.user.name }</Link>
+            By <Link href="/" className="font-semibold hover:text-gray-800">{ article.user?.name }</Link>
             , Published on <>{ article.createdAt }</>
           </p>
           <p className="pb-6">{ article.content.slice(0, displayArticleContent) }</p>
