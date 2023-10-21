8d10bf29af0d2016672870b0ab24bb272e256a88
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-22 00:37:26 +0900

    add: not found

--- src/app/[locale]/article/[id]/page.tsx
+++ src/app/[locale]/article/[id]/page.tsx
@@ -15,16 +15,20 @@ const ArticleDetail = async({
 
   const article: Article = res.data;
 
+  if (article === null) {
+    return <div>Article not found</div>
+  }
+
   return (
     <article className="flex flex-col shadow my-4">
       <Link href="/" className="hover:opacity-75">
         <img src={ article?.thumbnailUrl } alt={article?.title}/>
       </Link>
       <div className="bg-white flex flex-col justify-start p-6">
-        <Link href="/" className="text-blue-700 text-sm font-bold uppercase pb-4">{ article?.category.name }</Link>
+        <Link href="/" className="text-blue-700 text-sm font-bold uppercase pb-4">{ article?.category?.name }</Link>
         <p className="text-3xl font-bold hover:text-gray-700 pb-4">{ article?.title }</p>
         <p className="text-sm pb-3">
-          By <Link href="/" className="font-semibold hover:text-gray-800">{ article?.user.name }</Link>
+          By <Link href="/" className="font-semibold hover:text-gray-800">{ article?.user?.name }</Link>
           , Published on <>{ article?.createdAt }</>
         </p>
         <p className="pb-6">{ article?.content }</p>
