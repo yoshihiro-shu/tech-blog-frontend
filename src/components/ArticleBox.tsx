2d0e7c6d44aa79a94396ed1b692dcb6a7eb2ec29
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-28 01:39:06 +0900

    add: tag into aritlce box

--- src/components/ArticleBox.tsx
+++ src/components/ArticleBox.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from 'next-intl';
 
 import Article from '@/server/types/article'
 import { displayTime } from '@/src/lib/markdown/datetime';
-import { cagtegoryArticlesURL } from '@/src/lib/markdown/siteMap';
+import { cagtegoryArticlesURL, tagArticlesURL } from '@/src/lib/markdown/siteMap';
 import { Thumbnail } from '@/src/components/Thumbnail'
 
 type Props = { article: Article }
@@ -11,7 +11,6 @@ type Props = { article: Article }
 export const  ArticleBox = ({ article }: Props) => {
     const t = useTranslations("Article");
     const articleDetailPath: string = "/article/" + article.id
-
     return (
       <article className="flex flex-col shadow my-4 border-2 border-indigo-600">
         <Link href={articleDetailPath} className="hover:opacity-75 w-full">
@@ -19,7 +18,12 @@ export const  ArticleBox = ({ article }: Props) => {
         </Link>
         <div className="bg-white flex flex-col justify-start p-6 border-t-2 border-indigo-600">
           <Link href={cagtegoryArticlesURL(article.category?.slug)} className="text-blue-700 text-sm font-bold uppercase pb-4">{ article.category?.name }</Link>
-          <p className="text-sm pb-3">
+          <p className="text-sm flex justify-start">
+            {article.tags.map(tag => (
+              <Link href={tagArticlesURL(tag.slug)} className='mr-2'>#{ tag.name }</Link>
+            ))}
+          </p>
+          <p className="text-sm flex justify-end">
             {t('LastModifiedAt')}  <>{ displayTime(article.createdAt) }</>
           </p>
         </div>
