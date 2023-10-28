8f4da7dc95dfe838952155cedbb299297e166253
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-28 13:48:29 +0900

    add: article title component

--- src/components/Article/ArticleDetail.tsx
+++ src/components/Article/ArticleDetail.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 import Article from '@/server/types/article'
 import { cagtegoryArticlesURL } from '@/src/lib/markdown/siteMap';
 
+import ArticleTitleComponent from './ArticleTitle';
+import ArticleTagComponent from './ArticleTag';
 import MarkdownComponent from '../Markdown';
 import PublishedAtComponent from './PunlishedAt';
-import ArticleTagComponent from './ArticleTag';
 
 type Props = { article: Article }
 const ArticleDetail = async ({ article }: Props) => {
@@ -14,7 +15,7 @@ const ArticleDetail = async ({ article }: Props) => {
         <article className="flex flex-2 shadow my-4 md:w-3/4">
           {/* <TableOfContents content={article.content} /> */}
           <div className="bg-white p-6 w-full">
-            <div className="text-4xl font-bold hover:text-gray-700 pb-4">{ article?.title }</div>
+            <ArticleTitleComponent title={article.title} />
             <Link href={cagtegoryArticlesURL(article.category.slug)} className="text-blue-700 text-sm font-bold pb-4">{ article?.category?.name }</Link>
             <ArticleTagComponent tags={article.tags} />
             <MarkdownComponent content={article.content}/>
