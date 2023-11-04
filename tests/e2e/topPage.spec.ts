81c1e550d3d4bb077593520a3e437d7d784a5bdb
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-05 03:30:55 +0900

    add: github action

--- tests/e2e/topPage.spec.ts
+++ tests/e2e/topPage.spec.ts
@@ -7,6 +7,4 @@ test('should navigate to the profile page', async ({ page }) => {
   await page.getByText('プロフィール').click()
   // The new URL should be "/profile" (baseURL is used there)
   await expect(page).toHaveURL('http://localhost:3000/ja/profile')
-  // The new page should contain an h1 with "職務経歴書"
-  await expect(page.locator('h1')).toContainText('職務経歴書')
 })
