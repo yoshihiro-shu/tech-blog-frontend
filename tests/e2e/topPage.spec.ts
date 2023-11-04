9c4b8e9add426f74235c6a5b17584ef5a7807d58
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-05 03:24:12 +0900

    add: test should navigate to the profile page

--- tests/e2e/topPage.spec.ts
+++ tests/e2e/topPage.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test'
 test('should navigate to the profile page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('http://localhost:3000/')
-  // Find an element with the text 'About Page' and click on it
-//   await page.click('text=About')
-  // The new URL should be "/about" (baseURL is used there)
-//   await expect(page).toHaveURL('http://localhost:3000/about')
-//   // The new page should contain an h1 with "About Page"
-//   await expect(page.locator('h1')).toContainText('About Page')
+  // Find an element with the text 'プロフィール' and click on it
+  await page.getByText('プロフィール').click()
+  // The new URL should be "/profile" (baseURL is used there)
+  await expect(page).toHaveURL('http://localhost:3000/ja/profile')
+  // The new page should contain an h1 with "職務経歴書"
+  await expect(page.locator('h1')).toContainText('職務経歴書')
 })
