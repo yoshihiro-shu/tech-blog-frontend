2264da244f27385787d1cd80b9677adc1c43c259
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-05 03:08:06 +0900

    update: config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,27 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
 
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
 const config: PlaywrightTestConfig = {
-  testDir: "./tests",
+  // Timeout per test
+  timeout: 30 * 1000,
+  testDir: "./tests/e2e",
+  // If a test fails, retry it additional 2 times
+  retries: 2,
+  // Artifacts folder where screenshots, videos, and traces are stored.
+  outputDir: './tests/results/e2e',
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
+  },
+  // Run your local dev server before starting the tests:
+  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  webServer: {
+    command: 'yarn dev',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
   projects: [
     {
