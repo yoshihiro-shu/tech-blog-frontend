7443b589a357533687dfc55da4d6d8b6fa437f42
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-10 23:26:26 +0900

    replace: default seo

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,15 +2,15 @@
 // import { Inter } from 'next/font/google'
 import Script from 'next/script'
 
-import { NextSeo } from 'next-seo';
-import {getTranslator} from 'next-intl/server';
-import {ReactNode} from 'react';
+import { getTranslator } from 'next-intl/server';
+import { ReactNode } from 'react';
 
 import { NavBar} from '@/src/components/NavBar'
 import { Header } from '@/src/components/Header'
 import { TopicNav } from '@/src/components/TopicNav'
 import { SideBar} from '@/src/components/SideBar'
 import { Footer } from '@/src/components/Footer'
+
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -26,13 +26,40 @@ export async function generateMetadata({
 }: Omit<Props, 'children'>) {
   const t = await getTranslator(locale);
 
+  const siteName = t('MetaData.SiteName')
+  const siteURL = t('MetaData.SiteURL')
+  const siteDescription = t('MetaData.SiteDescription')
   return {
-    title: t('Header.title')
+    title: {
+      default: siteName,
+      template: `%s | ${siteName}`
+    },
+    siteDescription,
+    openGraph: {
+      title: siteName,
+      siteDescription,
+      siteURL,
+      siteName,
+      locale: 'ja_JP',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteName,
+      siteDescription,
+      site: '@iamyoshitter',
+      creator: '@iamyoshitter',
+    },
+    // verification: {
+    //   google: 'サーチコンソールのやつ',
+    // },
+    alternates: {
+      canonical: siteURL,
+    },
   };
 }
 
 export default async function LocaleLayout({children, params: {locale}}: Props){
-  const t = await getTranslator(locale);
   return (
     <html lang={locale}>
       {/* TODO FIX */}
@@ -48,10 +75,6 @@ export default async function LocaleLayout({children, params: {locale}}: Props){
           gtag('config', 'G-0EGNB55Y3V');
         `}
       </Script>
-      <NextSeo
-        title={t('Header.title')}
-        description={t('Header.description')}
-      />
       <body>
         <NavBar />
         <Header />
