4dc1b69ee2567fa43f44de86d09e092f2cc3daca
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-10 20:48:19 +0900

    fix: path

--- tests/e2e/topPage.spec.ts
+++ tests/e2e/topPage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should navigate to the profile page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('http://localhost:3000/')
+  await page.goto('http://localhost:3000/ja')
   // Find an element with the text 'プロフィール' and click on it
   await page.getByText('プロフィール').click()
   // The new URL should be "/profile" (baseURL is used there)
