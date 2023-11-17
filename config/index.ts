263cb7d6a9cdc2d4be632072703d62da652ad790
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-17 19:47:11 +0900

    add: @types/node

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,15 @@
 export default {
     // BackendAPI: process.env.BASE_URL || " http://localhost:8000/",
     backendAPI: process.env.BANCKED_API,
+    SNS: {
+        xLink: process.env.X_LINK || "",
+        instagramLink: process.env.INSTAGRAM_LINK || "",
+        facebookLink: process.env.FACEBOOK_LINK || "",
+        linkedinLink: process.env.LINKEDIN_LINK || "",
+        gmailLink: process.env.GMAIL_LINK || "",
+        thereadsLink: process.env.THEREADS_LINK || "",
+        githubLink: process.env.GITHUB_LINK || "",
+        dockerhubLink: process.env.DOCKERHUB_LINK || "",
+        qiitaLink: process.env.QIITA_LINK || "",
+    }
 }
