70ba14fbff84f7fa6d1f3edab64101e81e83518a
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-26 17:41:48 +0900

    add: comment out

--- src/app/api/metrics/route.ts
+++ src/app/api/metrics/route.ts
@@ -1,8 +1,15 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import register from '@/src/lib/prometheus';
 
+/**
+ * Retrieves metrics and returns a Response object with the metrics data and appropriate headers.
+ *
+ * @param {NextRequest} _ - The request object.
+ * @return {Promise<Response>} A promise that resolves to a Response object containing the metrics data.
+ */
 export async function GET(_: NextRequest) {
   const metrics = await register.metrics();
+  // NextResponse cannot set headers.So use Response
   return new Response(metrics, {
     headers: {
       'Content-Type': register.contentType
