152787d4361ed50fa07fbd1ecb0986c5aa59627c
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-26 17:33:48 +0900

    update: metrics

--- src/app/api/metrics/route.ts
+++ src/app/api/metrics/route.ts
@@ -2,9 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import register from '@/src/lib/prometheus';
 
 export async function GET(_: NextRequest) {
-  const res = new Response();
-  res.headers.set('Content-Type', register.contentType);
-  return res
+  const metrics = await register.metrics();
+  return new Response(metrics, {
+    headers: {
+      'Content-Type': register.contentType
+    }
+  });
 }
 
 export async function POST(_: NextRequest) {
