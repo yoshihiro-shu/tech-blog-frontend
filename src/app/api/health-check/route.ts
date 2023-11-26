bbce41735dc949500221aaa5461c460a01ff038e
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-11-26 16:51:23 +0900

    add: api of health check

---
+++ src/app/api/health-check/route.ts
@@ -0,0 +1,17 @@
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(_: NextRequest) {
+  return NextResponse.json({
+    status: 200,
+  })
+}
+
+export async function POST(_: NextRequest) {
+}
+
+export async function PUT(_: NextRequest) {
+}
+
+export async function DELETE(_: NextRequest) {
+}
