5f45d657d6207d509f71b6fe4748cfe6f0ee295a
yoshihiro-shu <yoihongzhu0321@gmail.com>
2024-03-01 21:29:51 +0900

    header titleをenvから参照する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useTranslations } from 'next-intl';
+import { SITE_NAME } from '../constants/siteName';
 
 export function Header() {
   const t = useTranslations("Header");
@@ -7,7 +8,7 @@ export function Header() {
       <header className="w-full container mx-auto">
         <div className="flex flex-col items-center py-12">
           <Link href="/" className="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl">
-            {t('title')}
+            {SITE_NAME}
           </Link>
           <br/>
           <p className="text-lg text-gray-600">
