0ca5edbf80b6efcee45119307c3178db6c6dbc59
yoshihiro-shu <yoihongzhu0321@gmail.com>
2024-06-01 23:36:16 +0900

    おりゃ！

--- src/app/(article)/tag/[slug]/page.tsx
+++ src/app/(article)/tag/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import ArticleList from '@/app/(article)/_components/article/List'
+import ArticleList from '@/app/(article)/_components/article/list'
 import ErrorPage from '@/app/_components/error';
 
 import Article from '@/server/types/article';
