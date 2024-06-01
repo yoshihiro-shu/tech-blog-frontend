d62e68a76506090fa9b0bda79d5e01f130285d1b
yoshihiro-shu <yoihongzhu0321@gmail.com>
2024-06-01 17:53:49 +0900

    fix

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -2,66 +2,17 @@
 // import { Inter } from 'next/font/google'
 import Script from 'next/script'
 
-import { ReactNode } from 'react';
-
 import { NavBar} from '@/src/components/NavBar'
 import { Header } from '@/src/components/Header'
 import { TopicNav } from '@/src/components/TopicNav'
 import { SideBar} from '@/src/components/SideBar'
 import { Footer } from '@/src/components/Footer'
-import { SITE_NAME, SITE_URL } from '@/src/constants/siteName';
 
 // const inter = Inter({ subsets: ['latin'] })
 
-type Props = {
-  children: ReactNode;
-  params: {
-    locale: string,
-  };
-};
-
-export async function generateMetadata({
-  params: {locale}
-}: Omit<Props, 'children'>) {
-
-  const siteName = SITE_NAME
-  const siteURL = SITE_URL
-  const siteDescription = "〜 学んだことを随時更新しています。 〜"
-  const twitterSite = "@yoshihiro_shu"
-  const twitterCreater = "@yoshihiro_shu"
-  return {
-    title: {
-      default: siteName,
-      template: `%s | ${siteName}`
-    },
-    siteDescription,
-    openGraph: {
-      title: siteName,
-      siteDescription,
-      siteURL,
-      siteName,
-      locale: 'ja_JP',
-      type: 'article',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: siteName,
-      siteDescription,
-      site: twitterSite,
-      creator: twitterCreater,
-    },
-    // verification: {
-    //   google: 'サーチコンソールのやつ',
-    // },
-    alternates: {
-      canonical: siteURL,
-    },
-  };
-}
-
-export default async function Layout({children, params: {locale}}: Props){
+export default async function Layout({ children }: { children: React.ReactNode }){
   return (
-    <html lang={locale}>
+    <html lang="ja">
       {/* TODO FIX */}
       {/* <body className={inter.className}> */}
       {/* Google tag (gtag.js)s */}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import { type Metadata } from "next";
 import {ReactNode} from 'react';
 import './globals.css'
+
+import { SITE_NAME, SITE_URL } from '@/src/constants/siteName';
 
 type Props = {
   children: ReactNode;
@@ -11,3 +14,38 @@ type Props = {
 export default function RootLayout({children}: Props) {
   return children;
 };
+
+const siteName = SITE_NAME
+const siteURL = SITE_URL
+const siteDescription = "〜 学んだことを随時更新しています。 〜"
+const twitterSite = "@yoshihiro_shu"
+const twitterCreater = "@yoshihiro_shu"
+
+export const metadata: Metadata = {
+    title: {
+      default: siteName,
+      template: `%s | ${siteName}`
+    },
+    description: siteDescription,
+    openGraph: {
+      title: siteName,
+      description: siteDescription,
+      url: siteURL,
+      siteName: siteName,
+      locale: 'ja_JP',
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteName,
+      description: siteDescription,
+      site: twitterSite,
+      creator: twitterCreater,
+    },
+    // verification: {
+    //   google: 'サーチコンソールのやつ',
+    // },
+    alternates: {
+      canonical: siteURL,
+    },
+  };
